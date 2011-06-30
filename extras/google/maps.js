9e36ac8c5b2e778b7d6c7a3546d33da7b2271418
Michel Blanc <mblanc@erasme.org>
2011-06-30 16:12:45 +0200

    updated googlemaps module & samples

--- extras/google/maps.js
+++ extras/google/maps.js
@@ -1,31 +1,30 @@
-Ui.Element.extend('Extras.Ui.Google.Map', {
-	latlng: undefined,
+document.write("<script type='text/javascript' src='http://maps.google.com/maps/api/js?sensor=false'></script>"); // v=3 ?
+
+Ui.Fixed.extend('Extras.Ui.Google.Map', {
 	map: undefined,
-	zoom: 10,
-	/*		panControl: false,
-			zoomControl: false,
-			mapTypeControl: false;
-	*/
 
 	constructor: function(config) {
-		if(('latitude' in config) && ('longitude' in config))
-			this.setLatLng(config.latitude, config.longitude);
+		var latlng = new google.maps.LatLng(0, 0);
+		var zoom = 10;
+        var maptype = google.maps.MapTypeId.ROADMAP;
+
+		if(('latitude' in config) && ('longitude' in config)) {
+			latlng = new google.maps.LatLng(config.latitude, config.longitude);
+            console.log('lat' + config.latitude + ', lon' + config.longitude);
+        }
 		if('zoom' in config)
-			this.setZoom(config.zoom);
+			zoom = config.zoom;
 		if('maptype' in config)
-			this.setMapType(config.maptype)
-		
-		var myOptions = 
-			{
-				zoom: this.zoom,
-				center: this.latlng,
-				mapTypeId: this.maptype,
-				noClear: true,
-			};
+            maptype =  config.maptype;
+
+		var myOptions = {
+			zoom: zoom,
+			center: latlng,
+			mapTypeId: maptype,
+		};
 		this.map = new google.maps.Map(this.getDrawing(), myOptions);
-		
-				// We don't have a copy of these, so we set them only
-		// when the map	 object is alive
+
+		// when the map object is alive
 		if ('panControl' in config)
 			this.showPanControl(config.panControl);
 		if ('zoomControl' in config)
@@ -34,8 +33,13 @@ Ui.Element.extend('Extras.Ui.Google.Map', {
 			this.showMapTypeControl(config.mapTypeControl);
 		if ('streetViewControl' in config)
 			this.showStreetViewControl(config.streetViewControl);
+
+        // Connect some events to tell google that the div has to be repainted
+		this.connect(this, 'load', this.updateSize);
+		this.connect(this, 'resize', this.updateSize);
+		this.connect(this, 'visible', this.updateSize);
 	},
-	
+
 	showPanControl: function(val) {
 		this.map.panControl = val;
 	},
@@ -53,32 +57,34 @@ Ui.Element.extend('Extras.Ui.Google.Map', {
 	},
 	
 	setLatLng: function(latitude, longitude) {
-		this.latlng = new google.maps.LatLng(latitude, longitude);	
-		if (this.map)
-			this.map.panTo(this.latlng);
-		// setCenter(latlng:LatLng) ??
+		this.map.setCenter(new google.maps.LatLng(latitude, longitude));
 	},
-	
-	setZoom: function(zoom) {
-		if ((zoom > 18) || (zoom < 0))
-			return;
-		this.zoom = zoom;
 
-		if (this.map)
-			this.map.setZoom(this.zoom);
-	},
-	
-	getZoom: function() {
-		return this.zoom;
-	},
-	
 	setMapType: function(type) {
 		this.maptype = type;
-		if (this.map)
+		if (this.map) {
+            console.log('setting maptype to ' + this.maptype);
 			this.map.setMapTypeId(this.maptype);
+        }
 	},
 	
 	getMap: function() {
 		return this.map;
+	},
+
+	getZoom: function() {
+		return this.map.getZoom();
+	},
+
+	setZoom: function(zoom) {
+		this.map.setZoom(zoom);
+	},
+
+	updateSize: function() {
+		google.maps.event.trigger(this.getMap(), 'resize');
 	}
 });
+
+
+
+
