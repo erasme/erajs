ff0946a2e6fb96da9e8bd9bee66fec1993868df0
Daniel Lacroix <dlacroix@erasme.org>
2011-07-18 14:24:09 +0200

    add getValue in CheckBox

--- era/js/ui/checkbox.js
+++ era/js/ui/checkbox.js
@@ -56,6 +56,10 @@ Ui.Togglable.extend('Ui.CheckBox',
 			this.setContent(config.content);
 	},
 
+	getValue: function() {
+		return this.getIsToggled();
+	},
+
 	setText: function(text) {
 		if(text == undefined) {
 			if(this.contentBox != undefined) {
