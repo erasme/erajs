cefc87fcf6fbdcba164fb8aed5679abf078dd246
Daniel Lacroix <dlacroix@erasme.org>
2011-08-30 10:51:27 +0200

    correct bug for IE < 9

--- era/js/ui/listview.js
+++ era/js/ui/listview.js
@@ -399,7 +399,7 @@ Ui.Pressable.extend('Ui.ListViewHeader',
 		var yuv = this.getStyleProperty('color').getYuv();
 		gradient = new Ui.LinearGradient({ stops: [
 			{ offset: 0, color: new Ui.Color({ y: yuv.y + 0.10, u: yuv.u, v: yuv.v }) },
-			{ offset: 1, color: new Ui.Color({ y: yuv.y - 0.10, u: yuv.u, v: yuv.v }) },
+			{ offset: 1, color: new Ui.Color({ y: yuv.y - 0.10, u: yuv.u, v: yuv.v }) }
 		] });
 		darkColor = new Ui.Color({ y: yuv.y - 0.30, u: yuv.u, v: yuv.v });
 		this.background.setFill(gradient);
