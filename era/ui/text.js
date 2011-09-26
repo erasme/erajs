0bbdc498d7deac729ed954ee61d99b71a1737b7b
Daniel Lacroix <dlacroix@erasme.org>
2011-09-26 17:07:51 +0200

    Improve perf for Ui.Text and allow styling

--- era/ui/text.js
+++ era/ui/text.js
@@ -7,17 +7,19 @@ Ui.Element.extend('Ui.Text',
 	// var needed to update the layout
 	line: undefined,
 	word: undefined,
+	words: undefined,
 	flowRender: false,
 	offsetX: 0,
 	offsetY: 0,
 	spaceWidth: undefined,
 	maxWidth: undefined,
 	lineHeight: undefined,
-	fontSize: 16,
-	fontFamily: 'Sans-serif',
-	fontWeight: 'normal',
-	color: 'black',
+	fontSize: undefined,
+	fontFamily: undefined,
+	fontWeight: undefined,
+	color: undefined,
 	selectable: false,
+	textMeasureValid: false,
 
     /**
      * @constructs
@@ -42,6 +44,7 @@ Ui.Element.extend('Ui.Text',
 	setText: function(text) {
 		if(this.text != text) {
 			this.text = text;
+			this.splitText();
 			this.invalidateMeasure();
 		}
 	},
@@ -49,37 +52,49 @@ Ui.Element.extend('Ui.Text',
 	setFontSize: function(fontSize) {
 		if(this.fontSize != fontSize) {
 			this.fontSize = fontSize;
-			this.textDrawing.style.fontSize = this.fontSize+'px';
+			this.textDrawing.style.fontSize = this.getFontSize()+'px';
+			this.textMeasureValid = false;
 			this.invalidateMeasure();
 		}
 	},
 
 	getFontSize: function() {
-		return this.fontSize;
+		if(this.fontSize !== undefined)
+			return this.fontSize;
+		else
+			return this.getStyleProperty('fontSize');
 	},
 
 	setFontFamily: function(fontFamily) {
 		if(this.fontFamily != fontFamily) {
 			this.fontFamily = fontFamily;
-			this.textDrawing.style.fontFamily = this.fontFamily;
+			this.textDrawing.style.fontFamily = this.getFontFamily();
+			this.textMeasureValid = false;
 			this.invalidateMeasure();
 		}
 	},
 
 	getFontFamily: function() {
-		return this.fontFamily;
+		if(this.fontFamily !== undefined)
+			return this.fontFamily;
+		else
+			return this.getStyleProperty('fontFamily');
 	},
 
 	setFontWeight: function(fontWeight) {
 		if(this.fontWeight != fontWeight) {
 			this.fontWeight = fontWeight;
-			this.textDrawing.style.fontWeight = this.fontWeight;
+			this.textDrawing.style.fontWeight = this.getFontWeight();
+			this.textMeasureValid = false;
 			this.invalidateMeasure();
 		}
 	},
 
 	getFontWeight: function() {
-		return this.fontWeight;
+		if(this.fontWeight !== undefined)
+			return this.fontWeight;
+		else
+			return this.getStyleProperty('fontWeight');
 	},
 
 	setColor: function(color) {
@@ -93,7 +108,10 @@ Ui.Element.extend('Ui.Text',
 	},
 
 	getColor: function() {
-		return this.color;
+		if(this.color !== undefined)
+			return this.color;
+		else
+			return Ui.Color.create(this.getStyleProperty('color'));
 	},
 
 	getSelectable: function() {
@@ -139,11 +157,9 @@ Ui.Element.extend('Ui.Text',
 	},
 
 	addWord: function(word) {
-		var wordSize = Ui.Label.measureText(word, this.fontSize, this.fontFamily, this.fontWeight).width;
-
-		if(wordSize > this.maxWidth)
-			this.maxWidth = wordSize;
-		var newOffsetX = this.offsetX + wordSize;
+		if(word.size > this.maxWidth)
+			this.maxWidth = word.size;
+		var newOffsetX = this.offsetX + word.size;
 
 		if(this.line != '')
 			newOffsetX += this.spaceWidth;
@@ -153,9 +169,8 @@ Ui.Element.extend('Ui.Text',
 			this.line += ' ';
 			this.offsetX += this.spaceWidth;
 		}
-		this.line += word;
-		this.offsetX += wordSize;
-		word = '';
+		this.line += word.word;
+		this.offsetX += word.size;
 	},
 
 	newLine: function() {
@@ -189,44 +204,71 @@ Ui.Element.extend('Ui.Text',
 		this.offsetX = 0;
 		this.offsetY = 0;
 		this.line = '';
-		this.spaceWidth = undefined;
 		var startPos = 0;
 		var word = '';
 		var i = 0;
+		for(i = 0; i < this.words.length; i++) {
+			var word = this.words[i];
+			if(word.type == 'word')
+				this.addWord(word);
+			else if(word.type == 'newline')
+				this.newLine();
+		}
+	},
 
-		this.spaceWidth = Ui.Label.measureText('x', this.fontSize, this.fontFamily, this.fontWeight).width;
-
+	splitText: function() {
+		var word = '';
+		var words = [];
 		for(i = 0; i < this.text.length; i++) {
 			if((this.text.charAt(i) == ' ') || (this.text.charAt(i) == '\n')) {
-
 				if(word != '') {
-					this.addWord(word);
+					words.push({ type: 'word', word: word });
 					word = '';
 				}
 				if(this.text.charAt(i) == '\n')
-					this.newLine();
+					words.push({ type: 'newline' });
 			}
 			else
 				word += this.text.charAt(i);
 		}
 		if(word != '')
-			this.addWord(word);
-
+			words.push({ type: 'word', word: word });
 		if(this.line != '')
-			this.newLine();
+			words.push({ type: 'newline' });
+		this.words = words;
+	},
+
+	updateMeasure: function() {
+		if(!this.textMeasureValid) {
+			this.spaceWidth = Ui.Label.measureText('x', this.getFontSize(), this.getFontFamily(), this.getFontWeight()).width;
+			for(var i = 0; i < this.words.length; i++) {
+				var word = this.words[i];
+				if(word.type == 'word')
+					word.size = Ui.Label.measureText(word.word, this.getFontSize(), this.getFontFamily(), this.getFontWeight()).width;
+			}
+			this.textMeasureValid = true;
+		}
 	}
 
 	/**#@-*/
 }, 
 /**@lends Ui.Text#*/
 {
+	onStyleChange: function() {
+		this.textDrawing.style.fontSize = this.getFontSize()+'px';
+		this.textDrawing.style.fontFamily = this.getFontFamily();
+		this.textDrawing.style.fontWeight = this.getFontWeight();
+		if(navigator.supportRgba)
+			this.textDrawing.style.color = this.getColor().getCssRgba();
+		else
+			this.textDrawing.style.color = this.getColor().getCssHtml();
+		this.textMeasureValid = false;
+		this.invalidateMeasure();
+	},
+
 	render: function() {
 		// create the container for all text rendering
 		this.textDrawing = document.createElement('div');
-		this.textDrawing.style.fontFamily = this.fontFamily;
-		this.textDrawing.style.fontWeight = this.fontWeight;
-		this.textDrawing.style.fontSize = this.fontSize+'px';
-		this.textDrawing.style.color = this.color;
 		this.textDrawing.style.position = 'absolute';
 		this.textDrawing.style.left = '0px';
 		this.textDrawing.style.top = '0px';
@@ -235,20 +277,26 @@ Ui.Element.extend('Ui.Text',
 	},
 
 	measureCore: function(width, height) {
-//		console.log(this+'.measureCore('+width+', '+height+')');
-
 		if(this.getWidth() != undefined)
 			width = this.getWidth();
+		this.updateMeasure();
 		this.updateFlow(width, false);
-//		console.log(this+'.measureCore('+width+', '+height+') = ('+this.maxWidth+'x'+this.offsetY+')');
 		return { width: this.maxWidth, height: this.offsetY };
 	},
 
 	arrangeCore: function(width, height) {
-//		console.log(this+'.arrangeCore('+width+', '+height+')');
 		// trash the previous layout
 		while(this.textDrawing.hasChildNodes())
 			this.textDrawing.removeChild(this.textDrawing.firstChild);
 		this.updateFlow(width, true);
+	}
+},
+/**@lends Ui.Text#*/
+{
+	style: {
+		color: new Ui.Color({ r: 0, g: 0, b: 0 }),
+		fontSize: 16,
+		fontFamily: 'Sans-serif',
+		fontWeight: 'normal'
 	}
 });
