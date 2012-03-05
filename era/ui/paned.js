b6db15b028ab8b5ddcc995b6d0d2b51ab8e4eb66
Daniel Lacroix <dlacroix@erasme.org>
2012-03-05 14:30:50 +0100

    add Ui.Paned to split screen and allow the user to set the split cursor

---
+++ era/ui/paned.js
@@ -0,0 +1,243 @@
+Ui.Container.extend('Ui.Paned', 
+/**@lends Ui.Paned#*/
+{
+	vertical: true,
+	cursor: undefined,
+	content1Box: undefined,
+	content1: undefined,
+	minContent1Size: 0,
+	content2Box: undefined,
+	content2: undefined,
+	minContent2Size: 0,
+	pos: 0.5,
+
+	/**
+	*	@constructs
+	*	@class
+	*	@extends Ui.Container
+	*/
+	constructor: function(config) {
+		this.content1Box = new Ui.LBox();
+		this.appendChild(this.content1Box);
+
+		this.content2Box = new Ui.LBox();
+		this.appendChild(this.content2Box);
+
+		this.cursor = new Ui.Movable();
+		this.appendChild(this.cursor);
+
+		this.cursor.setContent(new Ui.VPanedCursor());
+		this.connect(this.cursor, 'move', this.onCursorMove);
+
+		if(config.orientation != undefined)
+			this.setOrientation(config.orientation);
+	},
+
+	//
+	// Get the layout orientation.
+	// Possible values: [vertical|horizontal|
+	//
+	getOrientation: function() {
+		if(this.vertical)
+			return 'vertical';
+		else
+			return 'horizontal';
+	},
+
+	//
+	// Set the layout orientation.
+	// Possible values: [vertical|horizontal|
+	//
+	setOrientation: function(orientation) {
+		var vertical = true;
+		if(orientation != 'vertical')
+			vertical = false;
+		if(this.vertical != vertical) {
+			this.vertical = vertical;
+			if(this.vertical)
+				this.cursor.setContent(new Ui.VPanedCursor());
+			else
+				this.cursor.setContent(new Ui.HPanedCursor());
+			this.invalidateMeasure();
+		}
+	},
+
+	getContent1: function() {
+		return this.content1;
+	},
+
+	setContent1: function(content1) {
+		if(this.content1 != content1) {
+			if(this.content1 != undefined)
+				this.content1Box.remove(this.content1);
+			this.content1 = content1;
+			if(this.content1 != undefined)
+				this.content1Box.append(this.content1);
+		}
+	},
+
+	getContent2: function() {
+		return this.content2;
+	},
+
+	setContent2: function(content2) {
+		if(this.content2 != content2) {
+			if(this.content2 != undefined)
+				this.content2Box.remove(this.content2);
+			this.content2 = content2;
+			if(this.content2 != undefined)
+				this.content2Box.append(this.content2);
+		}
+	},
+
+	onCursorMove: function() {
+		this.disconnect(this.cursor, 'move', this.onCursorMove);
+		var p;
+		var aSize;
+		if(this.vertical) {
+			p = this.cursor.getPositionY();
+			aSize = this.getLayoutHeight() - this.cursor.getLayoutHeight();
+		}
+		else {
+			p = this.cursor.getPositionX();
+			aSize = this.getLayoutWidth() - this.cursor.getLayoutWidth();
+		}
+
+		this.pos = p / aSize;
+
+		if(aSize * this.pos < this.minContent1Size)
+			this.pos = this.minContent1Size / aSize;
+		if(aSize * (1 - this.pos) < this.minContent2Size)
+			this.pos = 1 - (this.minContent2Size / aSize);
+		p = this.pos * aSize;
+
+		if(p < 0)
+			p = 0;
+		if(p > aSize)
+			p = aSize;
+
+		if(this.vertical)
+			this.cursor.setPosition(0, p);
+		else
+			this.cursor.setPosition(p, 0);
+
+		this.invalidateMeasure();
+		this.connect(this.cursor, 'move', this.onCursorMove);
+	}
+}, 
+/**@lends Ui.Paned#*/
+{
+	measureCore: function(width, height) {
+		if(this.vertical) {
+			var cursorSize = this.cursor.measure(width, 0);
+
+			this.minContent1Size = this.content1Box.measure(width, 0).height;
+			this.minContent2Size = this.content2Box.measure(width, 0).height;
+
+			var content1Size = this.content1Box.measure(width, (height - cursorSize.height) * this.pos);
+			var content2Size = this.content2Box.measure(width, (height - cursorSize.height) * (1 - this.pos));
+
+			return { width: Math.max(cursorSize.width, Math.max(content1Size.width, content2Size.width)), height: content1Size.height + cursorSize.height + content2Size.height };
+		}
+		else {
+			var cursorSize = this.cursor.measure(0, height);
+
+			this.minContent1Size = this.content1Box.measure(0, 0).width;
+			this.minContent2Size = this.content2Box.measure(0, 0).width;
+
+			var content1Size = this.content1Box.measure((width - cursorSize.width) * this.pos, height);
+			var content2Size = this.content2Box.measure((width - cursorSize.width) * (1 - this.pos), height);
+
+			return { width: content1Size.width + cursorSize.width + content2Size.width, height: Math.max(cursorSize.height, Math.max(content1Size.height, content2Size.height)) };
+		}
+	},
+
+	arrangeCore: function(width, height) {
+		if(this.vertical) {
+			var cHeight = this.cursor.getMeasureHeight();
+			var aHeight = height - cHeight;
+
+			this.cursor.arrange(0, 0, width, cHeight);
+			this.cursor.setPosition(0, aHeight * this.pos);
+
+			this.content1Box.arrange(0, 0, width, aHeight * this.pos);
+			this.content2Box.arrange(0, (aHeight * this.pos) + cHeight, width, aHeight * (1 - this.pos));
+		}
+		else {
+			var cWidth = this.cursor.getMeasureWidth();
+			var aWidth = width - cWidth;
+
+			this.content1Box.arrange(0, 0, aWidth * this.pos, height);
+			this.cursor.arrange(0, 0, cWidth, height);
+			this.cursor.setPosition(aWidth * this.pos, 0);
+			this.content2Box.arrange((aWidth * this.pos) + cWidth, 0, aWidth * (1 - this.pos), height);
+		}
+	}
+});
+
+
+Ui.Paned.extend('Ui.VPaned',
+/**@lends Ui.VPaned#*/
+{
+	/**
+	*	@constructs
+	*	@class
+	*	@extends Ui.Paned
+	*/
+	constructor: function(config) {
+		this.setOrientation('vertical');
+	}
+});
+
+Ui.Paned.extend('Ui.HPaned', 
+/**@lends Ui.HPaned#*/
+{
+	/**
+	*	@constructs
+	*	@class
+	*	@extends Ui.Paned
+	*/
+	constructor: function(config) {
+		this.setOrientation('horizontal');
+	}
+});
+
+Ui.LBox.extend('Ui.HPanedCursor',
+/**@lends Ui.HPanedCursor#*/
+{
+	/**
+	*	@constructs
+	*	@class
+	*	@extends Ui.LBox
+	*/
+	constructor: function(config) {
+		var hbox = new Ui.HBox({ verticalAlign: 'center', height: 30 });
+		this.append(hbox);
+
+		hbox.append(new Ui.Rectangle({ fill: new Ui.Color({ r: 0, g: 0, b: 0, a: 0.6 }), width: 1, marginLeft: 10 }));
+		hbox.append(new Ui.Rectangle({ fill: new Ui.Color({ r: 0.31, g: 0.66, b: 1, a: 0.6 }), width: 1 }));
+
+		hbox.append(new Ui.Rectangle({ fill: new Ui.Color({ r: 0, g: 0, b: 0, a: 0.6 }), width: 1, marginLeft: 5 }));
+		hbox.append(new Ui.Rectangle({ fill: new Ui.Color({ r: 0.31, g: 0.66, b: 1, a: 0.6 }), width: 1, marginRight: 10 }));
+	}
+});
+
+Ui.LBox.extend('Ui.VPanedCursor',
+/**@lends Ui.VPanedCursor#*/
+{
+	/**
+	*	@constructs
+	*	@class
+	*	@extends Ui.LBox
+	*/
+	constructor: function(config) {
+		var vbox = new Ui.VBox({ horizontalAlign: 'center', width: 30 });
+		this.append(vbox);
+
+		vbox.append(new Ui.Rectangle({ fill: new Ui.Color({ r: 0, g: 0, b: 0, a: 0.6 }), height: 1, marginTop: 10 }));
+		vbox.append(new Ui.Rectangle({ fill: new Ui.Color({ r: 0.31, g: 0.66, b: 1, a: 0.6 }), height: 1 }));
+
+		vbox.append(new Ui.Rectangle({ fill: new Ui.Color({ r: 0, g: 0, b: 0, a: 0.6 }), height: 1, marginTop: 5 }));
+		vbox.append(new Ui.Rectangle({ fill: new Ui.Color({ r: 0.31, g: 0.66, b: 1, a: 0.6 }), height: 1, marginBottom: 10 }));
+	}
+});
