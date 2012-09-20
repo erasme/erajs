5325e4c9f691b4218bef6c7045ce89d81ca50fb3
Daniel Lacroix <dlacroix@erasme.org>
2012-09-20 15:13:44 +0200

    cleanup Ui.Uploadable and correct a bug in iOS 6

--- era/ui/uploadable.js
+++ era/ui/uploadable.js
@@ -1,7 +1,6 @@
-Ui.LBox.extend('Ui.Uploadable', 
+Ui.Pressable.extend('Ui.Uploadable', 
 /**@lends Ui.Uploadable#*/
 {
-	isDown: false,
 	content: undefined,
 	input: undefined,
 
@@ -16,7 +15,7 @@ Ui.LBox.extend('Ui.Uploadable',
 		this.setFocusable(true);
 		this.setRole('button');
 
-		this.addEvents('file', 'down', 'up');
+		this.addEvents('file');
 
 		if(navigator.isOpera || navigator.isFirefox3 || navigator.isIE7 || navigator.isIE8)
 			this.input = new Ui.UploadableWrapper();
@@ -25,17 +24,7 @@ Ui.LBox.extend('Ui.Uploadable',
 		this.append(this.input);
 		this.connect(this.input, 'file', this.onFile);
 
-		// handle mouse
-		this.connect(this.getDrawing(), 'mousedown', this.onMouseDown);
-
-		// handle touches
-		this.connect(this.getDrawing(), 'touchstart', this.onTouchStart);
-		this.connect(this.getDrawing(), 'touchmove', this.onTouchMove);
-		this.connect(this.getDrawing(), 'touchend', this.onTouchEnd);
-
-		// handle keyboard
-		this.connect(this.getDrawing(), 'keydown', this.onKeyDown);
-		this.connect(this.getDrawing(), 'keyup', this.onKeyUp);
+		this.connect(this, 'press', this.onPress);
 	},
 
 	setDirectoryMode: function(active) {
@@ -46,169 +35,13 @@ Ui.LBox.extend('Ui.Uploadable',
 	 * @private
 	 */
 
-	onMouseDown: function(event) {
-		if((event.button != 0) || this.getIsDisabled())
-			return;
-
-		event.preventDefault();
-		event.stopPropagation();
-
-		this.mouseStartX = event.screenX;
-		this.mouseStartY = event.screenY;
-
-		if('attachEvent' in this.getDrawing()) {
-			this.connect(this.getDrawing(), 'mousemove', this.onMouseMove, true);
-			this.connect(this.getDrawing(), 'mouseup', this.onMouseUp, true);
-		}
-		else {
-			this.connect(window, 'mousemove', this.onMouseMove, true);
-			this.connect(window, 'mouseup', this.onMouseUp, true);
-		}
-
-		this.onDown();
-	},
-
-	onMouseMove: function(event) {
-		var deltaX = event.screenX - this.mouseStartX;
-		var deltaY = event.screenY - this.mouseStartY;
-		var delta = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-
-		event.preventDefault();
-		event.stopPropagation();
-
-		// if the user move to much, release the touch event
-		if(delta > 10) {
-			this.onUp();
-
-			if('createEvent' in document) {
-				this.disconnect(this.getDrawing(), 'mousedown', this.onMouseDown);
-
-				var mouseDownEvent = document.createEvent('MouseEvents');
-				mouseDownEvent.initMouseEvent('mousedown', true, true, window, 1, event.screenX, event.screenY,
-					event.clientX, event.clientY,
-					event.ctrlKey, event.altKey, event.shiftKey,
-					event.metaKey, 0, event.target);
-				event.target.dispatchEvent(mouseDownEvent);
-
-				this.connect(this.getDrawing(), 'mousedown', this.onMouseDown);
-			}
-		}
-	},
-
-	getIsDown: function() {
-		return this.isDown;
-	},
-
-	onMouseUp: function(event) {
-		if(!this.isDown)
-			return;
-
-		event.preventDefault();
-		event.stopPropagation();
-		if(event.button == 0) {
-			this.onUp();
-			this.onPress();
-		}
-	},
-
-	onTouchStart: function(event) {
-		if(this.getIsDisabled())
-			return;
-		if(this.isDown) {
-			this.onUp();
-			return;
-		}
-		if(event.targetTouches.length != 1)
-			return;
-
-		event.preventDefault();
-		event.stopPropagation();
-
-		this.touchStartX = event.targetTouches[0].screenX;
-		this.touchStartY = event.targetTouches[0].screenY;
-		this.onDown();
-	},
-
-	onTouchMove: function(event) {
-		if(!this.isDown)
-			return;
-		
-		var deltaX = event.targetTouches[0].screenX - this.touchStartX;
-		var deltaY = event.targetTouches[0].screenY - this.touchStartY;
-		var delta = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-
-		// if the user move to much, release the touch event
-		if(delta > 10) {
-			this.onUp();
-
-			this.disconnect(this.getDrawing(), 'touchstart', this.onTouchStart);
-
-			var touchStartEvent = document.createEvent('TouchEvent');
-			touchStartEvent.initTouchEvent('touchstart', true, true, window, 0, 0, 0, 0, 0,
-				event.ctrlKey, event.altKey, event.shiftKey,
-				event.metaKey, event.touches,
-				event.targetTouches, event.changedTouches, event.scale, event.rotation);
-			event.target.dispatchEvent(touchStartEvent);
-
-			this.connect(this.getDrawing(), 'touchstart', this.onTouchStart);
-		}
-
-		event.preventDefault();
-		event.stopPropagation();
-	},
-	
-	onTouchEnd: function(event) {
-		if(!this.isDown)
-			return;
-
-		event.preventDefault();
-		event.stopPropagation();
-		this.onUp();
-		this.onPress();
-		this.focus();
-	},
-
-	onKeyDown: function(event) {
-		var key = event.which;
-		if((key == 13) && !this.getIsDisabled()) {
-			event.preventDefault();
-			event.stopPropagation();
-			this.onDown();
-		}
-	},
-
-	onKeyUp: function(event) {
-		var key = event.which;
-		if((key == 13) && !this.getIsDisabled()) {
-			event.preventDefault();
-			event.stopPropagation();
-			this.onUp();
-			this.onPress();
-		}
-	},
-
-	onDown: function() {
-		this.isDown = true;
-		this.focus();
-		this.fireEvent('down', this);
-	},
-
-	onUp: function() {
-		if('attachEvent' in this.getDrawing()) {
-			this.disconnect(this.getDrawing(), 'mousemove', this.onMouseMove);
-			this.disconnect(this.getDrawing(), 'mouseup', this.onMouseUp);
-		}
-		else {
-			this.disconnect(window, 'mousemove', this.onMouseMove);
-			this.disconnect(window, 'mouseup', this.onMouseUp);
-		}
- 		this.isDown = false;
-		this.fireEvent('up', this);
-	},
-
 	onPress: function() {
-		if(Ui.UploadableFileWrapper.hasInstance(this.input))
-			this.input.select();
+		if(Ui.UploadableFileWrapper.hasInstance(this.input)) {
+			// delay the task because of the iOS thread problem
+			var task = new Core.DelayedTask({ scope: this, delay: 0, callback: function() {
+				this.input.select();
+			} });
+		}
 	},
 
 	onFile: function(fileWrapper, file) {
