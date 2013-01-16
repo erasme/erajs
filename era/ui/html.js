72531c7703c71bb5b8c79449a23ff0ef237afb16
Daniel Lacroix <dlacroix@erasme.org>
2013-01-16 15:26:00 +0100

    add "link" event in Ui.Html to catch A link pressed

--- era/ui/html.js
+++ era/ui/html.js
@@ -3,6 +3,7 @@ Ui.Element.extend('Ui.Html',
 {
 	htmlDrawing: undefined,
 	html: undefined,
+	mouseTarget: undefined,
 
 	/**
 	 * @constructs
@@ -10,15 +11,53 @@ Ui.Element.extend('Ui.Html',
 	 * @extends Ui.Element
 	 */
 	constructor: function(config) {
+		this.addEvents('link');
 	},
 
 	getHtml: function() {
 		return this.htmlDrawing.innerHTML;
 	},
+	
+	getElements: function(tagName) {
+		var res = [];
+		this.searchElements(tagName.toUpperCase(), this.htmlDrawing, res);
+		return res;
+	},
+	
+	searchElements: function(tagName, element, res) {
+		for(var i = 0; i < element.childNodes.length; i++) {
+			var child = element.childNodes[i];
+			if(('tagName' in child) && (child.tagName.toUpperCase() == tagName))
+				res.push(child);
+			this.searchElements(tagName, child, res);
+		}
+	},
 
 	setHtml: function(html) {
+		//console.log(this+'.setHtml');
+		// remove old callbacks
+		var tab = this.getElements('A');
+		for(var i = 0; i < tab.length; i++) {
+			// handle mouse
+			this.disconnect(tab[i], 'mousedown', this.onLinkMouseDown);
+			this.disconnect(tab[i], 'click', this.onLinkClick);
+			// handle touches
+			this.disconnect(tab[i], 'fingerdown', this.onLinkFingerDown);
+		}
+		// update HTML content
 		this.htmlDrawing.innerHTML = html;
 		this.html = this.htmlDrawing.innerHTML;
+		// connect link callbacks
+		tab = this.getElements('A');
+		for(var i = 0; i < tab.length; i++) {
+			tab[i].htmlHref = tab[i].href;
+//			tab[i].removeAttribute('href');
+			// handle mouse
+			this.connect(tab[i], 'mousedown', this.onLinkMouseDown);
+			this.connect(tab[i], 'click', this.onLinkClick);
+			// handle touches
+			this.connect(tab[i], 'fingerdown', this.onLinkFingerDown);
+		}
 		this.invalidateMeasure();
 	},
 	
@@ -48,6 +87,134 @@ Ui.Element.extend('Ui.Html',
 			this.html = this.htmlDrawing.innerHTML;
 			this.invalidateMeasure();
 		}
+	},
+	
+	onLinkClick: function(event) {
+		event.preventDefault();
+		event.stopPropagation();
+	},
+	
+	onLinkMouseDown: function(event) {
+		if((event.button != 0) || this.getIsDisabled())
+			return;
+
+		event.preventDefault();
+		event.stopPropagation();
+		var target = event.target;
+		this.mouseTarget = target;
+
+//		console.log(this+'.onLinkMouseDown target: '+target);
+
+		target.mouseStartX = event.screenX;
+		target.mouseStartY = event.screenY;
+		target.isDown = true;
+
+		this.connect(window, 'mousemove', this.onLinkMouseMove, true);
+		this.connect(window, 'mouseup', this.onLinkMouseUp, true);
+	},
+
+	onLinkMouseMove: function(event) {
+		var target = this.mouseTarget;
+	
+		var deltaX = event.screenX - target.mouseStartX;
+		var deltaY = event.screenY - target.mouseStartY;
+		var delta = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+
+//		console.log(this+'.onLinkMouseMove delta: '+delta);
+
+		event.preventDefault();
+		event.stopPropagation();
+		
+		// if the user move to much, release the touch event
+		if(delta > 10) {
+			this.disconnect(window, 'mousemove', this.onLinkMouseMove, true);
+			this.disconnect(window, 'mouseup', this.onLinkMouseUp, true);
+			
+			target.isDown = false;
+
+			if('createEvent' in document) {
+				this.disconnect(target, 'mousedown', this.onLinkMouseDown);
+
+				var mouseDownEvent = document.createEvent('MouseEvents');
+				mouseDownEvent.initMouseEvent('mousedown', true, true, window, 1, event.screenX, event.screenY,
+					event.clientX, event.clientY,
+					event.ctrlKey, event.altKey, event.shiftKey,
+					event.metaKey, 0, event.target);
+				event.target.dispatchEvent(mouseDownEvent);
+
+				this.connect(target, 'mousedown', this.onLinkMouseDown);
+			}
+		}
+	},
+
+	onLinkMouseUp: function(event) {
+		var target = this.mouseTarget;
+		
+		if(!target.isDown)
+			return;
+		
+		event.preventDefault();
+		event.stopPropagation();
+
+		if(event.button == 0) {
+			this.disconnect(window, 'mousemove', this.onLinkMouseMove, true);
+			this.disconnect(window, 'mouseup', this.onLinkMouseUp, true);
+			this.fireEvent('link', this, target.htmlHref);
+		}
+	},
+	
+	onLinkFingerDown: function(event) {
+		var target = event.target;
+
+		if(this.getIsDisabled() || target.isDown)
+			return;
+
+		this.connect(event.finger, 'fingermove', this.onLinkFingerMove);
+		this.connect(event.finger, 'fingerup', this.onLinkFingerUp);
+
+		event.finger.capture(event.target);
+
+		event.preventDefault();
+		event.stopPropagation();
+
+		target.touchStartX = event.finger.getX();
+		target.touchStartY = event.finger.getY();
+		target.isDown = true;
+	},
+
+	onLinkFingerMove: function(event) {
+		var target = event.finger.getCaptureElement();
+	
+		event.preventDefault();
+		event.stopPropagation();
+
+		var deltaX = event.finger.getX() - target.touchStartX;
+		var deltaY = event.finger.getY() - target.touchStartY;
+		var delta = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+
+		// if the user move to much, release the touch event
+		if(delta > 10) {
+			this.disconnect(event.finger, 'fingermove', this.onLinkFingerMove);
+			this.disconnect(event.finger, 'fingerup', this.onLinkFingerUp);
+			target.isDown = false;
+
+			this.disconnect(target, 'fingerdown', this.onLinkFingerDown);
+			event.finger.release();
+			this.connect(target, 'fingerdown', this.onLinkFingerDown);
+		}
+	},
+	
+	onLinkFingerUp: function(event) {
+		var target = event.finger.getCaptureElement();
+	
+		this.disconnect(event.finger, 'fingermove', this.onLinkFingerMove);
+		this.disconnect(event.finger, 'fingerup', this.onLinkFingerUp);
+
+		event.preventDefault();
+		event.stopPropagation();
+
+		target.isDown = false;
+		this.fireEvent('link', this, target.htmlHref);
 	}
 
 }, {
@@ -69,12 +236,12 @@ Ui.Element.extend('Ui.Html',
 		div.style.position = 'absolute';
 		div.style.left = '0px';
 		div.style.top = '0px';
+		div.style.width = ((this.getWidth() != undefined)?Math.max(width,this.getWidth()):width)+'px';
 		div.innerHTML = this.htmlDrawing.innerHTML;
 		document.body.appendChild(div);
 		var needWidth = div.clientWidth;
 		var needHeight = div.clientHeight;
 		document.body.removeChild(div);
-
 		return { width: needWidth, height: needHeight };
 	},
 
