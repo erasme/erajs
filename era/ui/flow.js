cb44d10b1eefe20dfb2f199c8014849474d31e1b
Daniel Lacroix <dlacroix@erasme.org>
2013-02-14 11:20:00 +0100

    add spacing, itemAlign and prepend to Ui.Flow

--- era/ui/flow.js
+++ era/ui/flow.js
@@ -6,6 +6,8 @@ Ui.Container.extend('Ui.Flow',
 	uniform: false,
 	uniformWidth: undefined,
 	uniformHeight: undefined,
+	itemAlign: 'left',
+	spacing: 0,
 
 	/**
 	 * @constructs
@@ -15,10 +17,14 @@ Ui.Container.extend('Ui.Flow',
 	constructor: function(config) {
 	},
 
+	/*
+	 * Replace all item by the given one or the
+	 * array of given items
+	 */
 	setContent: function(content) {
 		while(this.getFirstChild() != undefined)
 			this.removeChild(this.getFirstChild());
-		if((content != undefined) && (typeof(content) == 'object')) {
+		if((content !== undefined) && (typeof(content) == 'object')) {
 			if(content.constructor == Array) {
 				for(var i = 0; i < content.length; i++)
 					this.appendChild(Ui.Element.create(content[i]));
@@ -27,9 +33,45 @@ Ui.Container.extend('Ui.Flow',
 				this.appendChild(Ui.Element.create(content));
 		}
 	},
+	
+	/*
+	 * Return the space between each item and each line
+	 */
+	getSpacing: function() {
+		return this.spacing;
+	},
+	
+	/*
+	 * Set the space between each item and each line
+	 */
+	setSpacing: function(spacing) {
+		if(this.spacing != spacing) {
+			this.spacing = spacing;
+			this.invalidateMeasure();
+			this.invalidateArrange();
+		}
+	},
+	
+	/*
+	 * Return item horizontal alignment [left|right]
+	 */
+	getItemAlign: function() {
+		return this.itemAlign;
+	},
+	
+	/*
+	 * Choose howto horizontaly align items [left|right]
+	 */
+	setItemAlign: function(itemAlign) {
+		if(itemAlign != this.itemAlign) {
+			this.itemAlign = itemAlign;
+			this.invalidateMeasure();
+			this.invalidateArrange();
+		}
+	},
 
 	/**
-	 * True if all children will be arrange to have the
+	 * true if all children will be arrange to have the
 	 * same width and height
 	 */
 	getUniform: function() {
@@ -55,12 +97,23 @@ Ui.Container.extend('Ui.Flow',
 	},
 
 	/**
+	 * Append a child at the begining of the flow
+	 */
+	prepend: function(child) {
+		this.prependChild(child);
+	},
+
+	/**
 	 * Append a child at the given position
 	 */
 	insertAt: function(child, position) {
 		this.insertChildAt(child, position);
 	},
 
+	/*
+	 * Move a given item from its current position to
+	 * the given one
+	 */
 	moveAt: function(child, position) {
 		this.moveChildAt(child, position);
 	},
@@ -84,10 +137,10 @@ Ui.Container.extend('Ui.Flow',
 			var child = this.getChildren()[i];
 			var size = child.measure(width, height);
 			if((ctx.lineX != 0) && (ctx.lineX + size.width > width)) {
-				line.width = ctx.lineX;
+				line.width = ctx.lineX - this.spacing;
 				line.height = ctx.lineHeight;
 				ctx.lineX = 0;
-				ctx.lineY += ctx.lineHeight;
+				ctx.lineY += ctx.lineHeight + this.spacing;
 				ctx.lineHeight = 0;
 
 				ctx.lineCount++;
@@ -95,7 +148,7 @@ Ui.Container.extend('Ui.Flow',
 			}
 			child.flowLine = line;
 			child.flowLineX = ctx.lineX;
-			ctx.lineX += size.width;
+			ctx.lineX += size.width + this.spacing;
 			if(size.height > ctx.lineHeight)
 				ctx.lineHeight = size.height;
 			if(ctx.lineX > ctx.minWidth)
@@ -119,14 +172,18 @@ Ui.Container.extend('Ui.Flow',
 			if(size.height > maxHeight)
 				maxHeight = size.height;
 		}
-		var countPerLine = Math.max(Math.floor(width / maxWidth), 1);
+		var countPerLine = Math.max(Math.floor((width+this.spacing)/(maxWidth+this.spacing)), 1);
+		
 		var nbLine = Math.ceil(this.getChildren().length / countPerLine);
 
 		for(var i = 0; i < this.getChildren().length; i++)
 			this.getChildren()[i].measure(maxWidth, maxHeight);
 		this.uniformWidth = maxWidth;
 		this.uniformHeight = maxHeight;
-		return { width: maxWidth * countPerLine, height: nbLine * maxHeight };
+		return {
+			width: maxWidth*countPerLine + (countPerLine-1)*this.spacing,
+			height: nbLine*maxHeight + (nbLine-1)*this.spacing
+		};
 	}
 	/**#@-*/
 }, 
@@ -135,8 +192,6 @@ Ui.Container.extend('Ui.Flow',
 	measureCore: function(width, height) {
 		if(this.getChildren().length == 0)
 			return { width: 0, height: 0 };
-
-//		console.log(this+'.measureCore('+width+','+height+')');
 		if(this.uniform)
 			return this.measureChildrenUniform(width, height);
 		else
@@ -145,22 +200,50 @@ Ui.Container.extend('Ui.Flow',
 
 	arrangeCore: function(width, height) {
 		if(this.uniform) {
-			var x = 0;
-			var y = 0; 
-			for(var i = 0; i < this.getChildren().length; i++) {
-				var child = this.getChildren()[i];
-				if(x + this.uniformWidth > width) {
-					x = 0;
-					y += this.uniformHeight;
+			if(this.itemAlign == 'left') {
+				var x = 0;
+				var y = 0;
+				for(var i = 0; i < this.getChildren().length; i++) {
+					var child = this.getChildren()[i];
+					if(x + this.uniformWidth > width) {
+						x = 0;
+						y += this.uniformHeight + this.spacing;
+					}
+					child.arrange(x, y, this.uniformWidth, this.uniformHeight);
+					x += this.uniformWidth + this.spacing;
 				}
-				child.arrange(x, y, this.uniformWidth, this.uniformHeight);
-				x += this.uniformWidth;
+			}
+			else if(this.itemAlign == 'right') {
+				var x;
+				var nbItemPerLine = Math.max(Math.floor((width+this.spacing)/(this.uniformWidth+this.spacing)), 1);
+				var lineWidth = nbItemPerLine*this.uniformWidth + (nbItemPerLine-1)*this.spacing;
+				
+				if(this.getChildren().length < nbItemPerLine)
+					x = width - ((this.getChildren().length*(this.uniformWidth+this.spacing)) - this.spacing);
+				else
+					x = width - lineWidth;
+				var y = 0;
+				for(var i = 0; i < this.getChildren().length; i++) {
+					var child = this.getChildren()[i];
+					if(x + this.uniformWidth > width) {
+						if(this.getChildren().length - i < nbItemPerLine)
+							x = width - (((this.getChildren().length-i)*(this.uniformWidth+this.spacing)) - this.spacing);
+						else
+							x = width - lineWidth;
+						y += this.uniformHeight + this.spacing;
+					}
+					child.arrange(x, y, this.uniformWidth, this.uniformHeight);
+					x += this.uniformWidth + this.spacing;
+				}
 			}
 		}
 		else {
 			for(var i = 0; i < this.getChildren().length; i++) {
 				var child = this.getChildren()[i];
-				child.arrange(child.flowLineX, child.flowLine.y, child.getMeasureWidth(), child.flowLine.height);
+				if(this.itemAlign == 'left')
+					child.arrange(child.flowLineX, child.flowLine.y, child.getMeasureWidth(), child.flowLine.height);
+				else
+					child.arrange(child.flowLineX+(width - child.flowLine.width), child.flowLine.y, child.getMeasureWidth(), child.flowLine.height);
 			}
 		}
 	}
