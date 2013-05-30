630c870416bfd2026d1c0f6eab6b53f8028c2189
Daniel Lacroix <dlacroix@erasme.org>
2013-05-30 12:36:16 +0200

    comment unused code

--- era/ui/draggable.js
+++ era/ui/draggable.js
@@ -106,13 +106,13 @@ Ui.LBox.extend('Ui.Draggable',
 		var uri = new Core.Uri({ uri: url });
 		this.downloadUrl = uri.toString();
 		// extract the fileName if not given
-		if(filename === undefined) {
+		//if(filename === undefined) {
 			// TODO
-		}
+		//}
 		// guess the mimetype if not given
-		if(mimetype === undefined) {
+		//if(mimetype === undefined) {
 			// TODO
-		}
+		//}
 	 	this.downloadMimetype = mimetype;
 		this.downloadFilename = filename;
 	},
