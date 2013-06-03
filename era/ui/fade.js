0b64af3d03f018a67001e330a198b8fa3c6ab044
Daniel Lacroix <dlacroix@erasme.org>
2013-06-03 15:25:23 +0200

    in Ui.Fade transition. Setup fade away to be 3 times faster than fade in

--- era/ui/fade.js
+++ era/ui/fade.js
@@ -5,14 +5,14 @@ Ui.Transition.extend('Ui.Fade',
 },
 /**@lends Ui.Fade#*/ 
 {
-	run: function(current, next, progress) {
+	run: function(current, next, progress) {	
 		if(current != undefined) {
 			if(progress == 1) {
-				current.setOpacity(1);
 				current.hide();
+				current.setOpacity(1);
 			}
 			else
-				current.setOpacity(1 - progress);
+				current.setOpacity(Math.min(1, Math.max(0, 1 - progress*3)));
 		}
 		if(next != undefined)
 			next.setOpacity(progress);
