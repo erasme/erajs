6a804bc66c8299e615d524bd0ee94427a8ebd858
Daniel Lacroix <dlacroix@erasme.org>
2013-09-16 17:07:32 +0200

    change the design of Ui.Slider for a light one

--- era/ui/slider.js
+++ era/ui/slider.js
@@ -13,26 +13,11 @@ Ui.Container.extend('Ui.Slider',
 	constructor: function(config) {
 		this.addEvents('change');
 
-		this.lightShadow = new Ui.Rectangle({ fill: new Ui.Color({ r: 1, g: 1, b: 1, a: 0.25 }), radius: 4 });
-		this.appendChild(this.lightShadow);
-
-		this.darkShadow = new Ui.Rectangle({ fill: new Ui.Color({ r: 0, g: 0, b: 0, a: 0.4}), radius: 4 });
-		this.appendChild(this.darkShadow);
-
-		this.background = new Ui.Rectangle({ fill: new Ui.Color({ r: 0.85, g: 0.85, b: 0.85 }), radius: 4 });
+		this.background = new Ui.Frame({ frameWidth: 2, radius: 4 });
 		this.appendChild(this.background);
 
-		this.backgroundShadow = new Ui.Shadow({ shadowWidth: 2, inner: true, radius: 4, opacity: 0.2 });
-		this.appendChild(this.backgroundShadow);
-
-		this.barBox = new Ui.LBox();
-		this.appendChild(this.barBox);
-
-		this.barBackground = new Ui.Rectangle({ radius: 3 });
-		this.barBox.append(this.barBackground);
-
-		this.bar = new Ui.Rectangle({ margin: 1, radius: 3 });
-		this.barBox.append(this.bar);
+		this.bar = new Ui.Rectangle({ margin: 1, height: 4 });
+		this.appendChild(this.bar);
 
 		this.button = new Ui.Movable({ moveVertical: false });
 		this.appendChild(this.button);
@@ -42,7 +27,7 @@ Ui.Container.extend('Ui.Slider',
 		this.connect(this.button, 'down', this.updateColors);
 		this.connect(this.button, 'up', this.updateColors);
 
-		this.buttonContent = new Ui.SliderContentDrawing({ marginTop: 5, marginLeft: 10, marginRight: 10});
+		this.buttonContent = new Ui.SliderContentDrawing({ marginTop: 10, marginBottom: 2, marginLeft: 12, marginRight: 12});
 		this.button.setContent(this.buttonContent);
 	},
 
@@ -86,25 +71,12 @@ Ui.Container.extend('Ui.Slider',
 		var maxX = width - 44;
 		this.button.setPosition(maxX * this.value, undefined);
 		var y = (height - 44)/2;
-		this.barBox.arrange(18, y + 18, (width - 36) * this.value, 9);
+		this.bar.arrange(18, y + 18, (width - 36) * this.value, 9);
 	},
 
-	getGradient: function() {
+	getColor: function() {
 		var yuv = this.getStyleProperty('color').getYuv();
-		return new Ui.LinearGradient({ stops: [
-			{ offset: 0, color: new Ui.Color({ y: yuv.y + 0.10, u: yuv.u, v: yuv.v }) },
-			{ offset: 1, color: new Ui.Color({ y: yuv.y - 0.10, u: yuv.u, v: yuv.v }) }
-		] });
-	},
-
-	getBarBorderColor: function() {
-		var yuv = this.getStyleProperty('color').getYuv();
-		return new Ui.Color({ y: yuv.y - 0.20, u: yuv.u, v: yuv.v });
-	},
-
-	getContentBorderColor: function() {
-		var yuv = this.getStyleProperty('color').getYuv();
-		return new Ui.Color({ y: yuv.y - 0.40, u: yuv.u, v: yuv.v });
+		return new Ui.Color({ y: yuv.y, u: yuv.u, v: yuv.v });
 	},
 
 	getButtonColor: function() {
@@ -120,8 +92,8 @@ Ui.Container.extend('Ui.Slider',
 	},
 
 	updateColors: function() {
-		this.bar.setFill(this.getGradient());
-		this.barBackground.setFill(this.getBarBorderColor());
+		this.bar.setFill(this.getColor());
+		this.background.setFill(this.getColor());
 		this.buttonContent.setFill(this.getButtonColor());
 	}
 
@@ -130,21 +102,15 @@ Ui.Container.extend('Ui.Slider',
 /**@lends Ui.Slider#*/
 {
 	measureCore: function(width, height) {
-		this.lightShadow.measure(width - 34, 10);
-		this.darkShadow.measure(width - 34, 10);
 		this.background.measure(width - 36, 10);
-		this.backgroundShadow.measure(width - 36, 10);
-		this.barBox.measure(width - 38, 9);
+		this.bar.measure(width - 38, 9);
 		this.button.measure(40, 40);
 		return { width: 88, height: 44 };
 	},
 
 	arrangeCore: function(width, height) {
 		var y = (height - 44)/2;
-		this.lightShadow.arrange(17, y + 18, width - 34, 10);
-		this.darkShadow.arrange(17, y + 17, width - 34, 10);
 		this.background.arrange(18, y + 18, width - 36, 10);
-		this.backgroundShadow.arrange(18, y + 18, width - 36, 10);
 		this.button.arrange(2, y + 2, 40, 40);
 		this.updateValue();
 	},
@@ -166,7 +132,7 @@ Ui.Container.extend('Ui.Slider',
 /**@lends Ui.Slider*/
 {
 	style: {
-		color: new Ui.Color({ r: 0.31, g: 0.66, b: 1 })
+		color: Ui.Color.create('#b1b1b1')
 	}
 });
 
@@ -179,7 +145,7 @@ Ui.LBox.extend('Ui.SliderContentDrawing',
 	shadow: undefined,
 	background: undefined,
 
-	radius: 8,
+	radius: 0,
 	fill: 'black',
 
 	/**
@@ -204,29 +170,15 @@ Ui.LBox.extend('Ui.SliderContentDrawing',
 	setFill: function(fill) {
 		if(this.fill != fill) {
 			this.fill = Ui.Color.create(fill);
-//			if(this.svgGradient != undefined)
-//				this.contentDrawing.removeChild(this.svgGradient);
-
 			var yuv = this.fill.getYuv();
-			var gradient = new Ui.LinearGradient({ stops: [
-				{ offset: 0, color: new Ui.Color({ y: yuv.y + 0.2, u: yuv.u, v: yuv.v }) },
-				{ offset: 1, color: new Ui.Color({ y: yuv.y - 0.1, u: yuv.u, v: yuv.v }) }
-			] });
-//			this.svgGradient = gradient.getSVGGradient();
-//			var gradId = 'grad'+Core.Util.generateId();
-//			this.svgGradient.setAttributeNS(null, 'id', gradId);
-//			this.contentDrawing.insertBefore(this.svgGradient, this.contentDrawing.firstChild);
-			this.background.setFill(gradient);
-//			this.background.style.fill = 'url(#'+gradId+')';
-//			this.shadow.style.fill = (new Ui.Color({ y: yuv.y - 0.9, u: yuv.u, v: yuv.v })).getCssHtml();
-			this.shadow.setFill((new Ui.Color({ y: yuv.y - 0.9, u: yuv.u, v: yuv.v })).getCssHtml());
+			this.background.setFill(this.fill);
+			this.shadow.setFill((new Ui.Color({ y: yuv.y - 0.3, u: yuv.u, v: yuv.v })).getCssHtml());
 		}
 	},
 
 	/**@private*/
 	genPath: function(width, height, radius) {
 		return 'M'+radius+',0 L'+(width-radius)+',0 Q'+width+',0 '+width+','+radius+'  L'+width+','+(height*0.66)+' L'+(width/2)+','+height+' L0,'+(height*0.66)+' L0,'+radius+' Q0,0 '+radius+',0 z';
-//		return 'M'+radius+',0 L'+(width-radius)+',0 A'+radius+','+radius+' 0 0,1 '+width+','+radius+'  L'+width+','+(height*0.66)+' L'+(width/2)+','+height+' L0,'+(height*0.66)+' L0,'+radius+' A'+radius+','+radius+' 0 0,1 '+radius+',0 z';
 	}
 }, 
 /**@lends Ui.SliderContentDrawing#*/
@@ -236,10 +188,6 @@ Ui.LBox.extend('Ui.SliderContentDrawing',
 
 		this.shadow.setPath(this.genPath(width, height, this.radius));
 		this.background.setPath(this.genPath(width-2, height-2, this.radius-1.4));
-
-//		this.shadow.setAttributeNS(null, 'd', this.genPath(width, height, this.radius));
-//		this.background.setAttributeNS(null, 'transform', 'translate(1,1)');
-//		this.background.setAttributeNS(null, 'd', this.genPath(width-2, height-2, this.radius-1.4));
 	}
 });
 
