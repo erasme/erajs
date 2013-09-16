79758c74d8c9d971ba6b106c788f637d2f0d6ead
Daniel Lacroix <dlacroix@erasme.org>
2013-09-16 17:13:29 +0200

    change the design of Ui.Locator for a lighter one and more compact

--- era/ui/locator.js
+++ era/ui/locator.js
@@ -1,10 +1,10 @@
+
 Ui.Container.extend('Ui.Locator', 
 /**@lends Ui.Locator#*/
 {
 	path: undefined,
 	foregrounds: undefined,
 	backgrounds: undefined,
-	spacing: 2,
 	border: undefined,
 
 	/**
@@ -14,6 +14,8 @@ Ui.Container.extend('Ui.Locator',
 	 */
 	constructor: function(config) {
 		this.addEvents('change');
+		this.connect(this, 'focus', this.updateColors);
+		this.connect(this, 'blur', this.updateColors);
 	},
 
 	setPath: function(path) {
@@ -22,24 +24,18 @@ Ui.Container.extend('Ui.Locator',
 		while(this.getChildren().length > 0)
 			this.removeChild(this.getChildren()[0]);
 
-		this.border = new Ui.Frame({ frameWidth: 1, fill: '#888888', radius: 8 });
+		this.border = new Ui.Rectangle({ fill: '#888888' });
 		this.appendChild(this.border);
 
 		this.backgrounds = [];
 		this.foregrounds = [];
 
 		var button;
-
-		var gradient = this.getGradient();
+		var spacing = this.getStyleProperty('spacing');
+		var fillColor = this.getColor();
 
 		if(path == '/') {
-			var bg = new Ui.Pressable();
-			bg.locatorPath = '/';
-			bg.locatorPos = 0;
-			this.connect(bg, 'press', this.onPathPress);
-			this.connect(bg, 'down', this.onPathDown);
-			this.connect(bg, 'up', this.onPathUp);
-			bg.appendChild(new Ui.Rectangle({ fill: gradient, radius: 7 }));
+			var bg = new Ui.Rectangle();
 			this.backgrounds.push(bg);
 			this.appendChild(bg);
 
@@ -68,18 +64,22 @@ Ui.Container.extend('Ui.Locator',
 			}
 			paths = cleanPaths;
 			
-			var currentPath = '/';
-
-			var bg = new Ui.Pressable();
-			bg.locatorPath = currentPath;
-			bg.locatorPos = 0;
-			this.connect(bg, 'press', this.onPathPress);
-			this.connect(bg, 'down', this.onPathDown);
-			this.connect(bg, 'up', this.onPathUp);
-			bg.appendChild(new Ui.LocatorRightArrow({ fill: gradient, radius: 7 }));
+			// create all bgs
+			var bg = new Ui.LocatorRightArrow({ arrowLength: spacing });
 			this.backgrounds.push(bg);
 			this.appendChild(bg);
 
+			for(var i = 0; i < paths.length; i++) {
+				var bg;
+				if(i == paths.length -1)
+					bg = new Ui.LocatorLeftArrow({ arrowLength: spacing });
+				else
+					bg = new Ui.LocatorLeftRightArrow({ arrowLength: spacing });
+				this.backgrounds.push(bg);
+				this.appendChild(bg);
+			}
+
+			// handle pressable parts
 			var fg = new Ui.Pressable();
 			fg.locatorPath = currentPath;
 			fg.locatorPos = 0;
@@ -95,23 +95,9 @@ Ui.Container.extend('Ui.Locator',
 
 			this.foregrounds.push(fg);
 			this.appendChild(fg);
-
+			var currentPath = '/';
 			for(var i = 0; i < paths.length; i++) {
-				currentPath += paths[i];
-
-				var bg = new Ui.Pressable();
-				bg.locatorPath = currentPath;
-				bg.locatorPos = i+1;
-				this.connect(bg, 'press', this.onPathPress);
-				this.connect(bg, 'down', this.onPathDown);
-				this.connect(bg, 'up', this.onPathUp);
-				if(i == paths.length -1)
-					bg.appendChild(new Ui.LocatorLeftArrow({ fill: gradient, radius: 7 }));
-				else
-					bg.appendChild(new Ui.LocatorLeftRightArrow({ fill: gradient, radius: 7 }));
-				this.backgrounds.push(bg);
-				this.appendChild(bg);
-	
+				currentPath += paths[i];	
 				var fg = new Ui.Pressable();
 				fg.locatorPos = i+1;
 				this.connect(fg, 'press', this.onPathPress);
@@ -121,7 +107,6 @@ Ui.Container.extend('Ui.Locator',
 				fg.appendChild(new Ui.Label({ text: paths[i], margin: 7 }));
 				this.foregrounds.push(fg);
 				this.appendChild(fg);
-
 				currentPath += '/';
 			}
 		}
@@ -131,18 +116,35 @@ Ui.Container.extend('Ui.Locator',
 		return this.path;
 	},
 
-	getGradient: function() {
-		return new Ui.LinearGradient({ stops: [
-			{ offset: 0, color: '#fbfbfb' },
-			{ offset: 1, color: '#c8c8c8' }
-		] });
+	getColor: function() {
+		return Ui.Color.create(this.getStyleProperty('color'));
 	},
-
-	getDownGradient: function() {
-		return new Ui.LinearGradient({ stops: [
-			{ offset: 0, color: '#bbbbbb' },
-			{ offset: 1, color: '#989898' }
-		] });
+	
+	getDarkColor: function() {
+		var yuv = this.getColor().getYuv();
+		var deltaY = 0;
+		if(yuv.y < 0.4)
+			return new Ui.Color({ y: yuv.y - 0.60 + deltaY, u: yuv.u, v: yuv.v, a: 0.8 });
+		else
+			return new Ui.Color({ y: yuv.y - 0.40 + deltaY, u: yuv.u, v: yuv.v, a: 0.4 });
+	},
+	
+	getLightColor: function() {
+		var yuv = this.getColor().getYuv();
+		var deltaY = 0;
+		if(yuv.y < 0.4)
+			return new Ui.Color({ y: yuv.y - 0.15 + deltaY, u: yuv.u, v: yuv.v });
+		else
+			return new Ui.Color({ y: yuv.y + 0.15 + deltaY, u: yuv.u, v: yuv.v });
+	},
+	
+	getDownColor: function() {
+		var yuv = this.getColor().getYuv();
+		var deltaY = -0.20;
+		if(yuv.y < 0.4)
+			return new Ui.Color({ y: yuv.y - 0.15 + deltaY, u: yuv.u, v: yuv.v });
+		else
+			return new Ui.Color({ y: yuv.y + 0.15 + deltaY, u: yuv.u, v: yuv.v });
 	},
 
 	onPathPress: function(pathItem) {
@@ -150,11 +152,21 @@ Ui.Container.extend('Ui.Locator',
 	},
 
 	onPathDown: function(pathItem) {
-		this.backgrounds[pathItem.locatorPos].getChildren()[0].setFill(this.getDownGradient());
+		this.backgrounds[pathItem.locatorPos].setFill(this.getDownColor());
 	},
 
 	onPathUp: function(pathItem) {
-		this.backgrounds[pathItem.locatorPos].getChildren()[0].setFill(this.getGradient());
+		this.backgrounds[pathItem.locatorPos].setFill(this.getLightColor());
+	},
+	
+	updateColors: function() {
+		var lightColor = this.getLightColor();		
+		this.border.setFill(this.getDarkColor());
+		for(var i = 0; i < this.backgrounds.length; i++) {
+			var bg = this.backgrounds[i];
+			console.log(bg);
+			bg.setFill(lightColor);
+		}
 	}
 }, 
 /**@lends Ui.Locator#*/
@@ -171,7 +183,7 @@ Ui.Container.extend('Ui.Locator',
 		this.border.measure(0, 0);
 
 		if(this.foregrounds.length == 1)
-			return { width: this.foregrounds[0].getMeasureWidth()+2, height: this.foregrounds[0].getMeasureHeight()+2 };
+			return { width: this.foregrounds[0].getMeasureWidth()+6, height: this.foregrounds[0].getMeasureHeight()+6 };
 		else {
 			var minWidth = 0;
 			var minHeight = 0;
@@ -181,7 +193,8 @@ Ui.Container.extend('Ui.Locator',
 					minHeight = child.getMeasureHeight();
 				minWidth += child.getMeasureWidth();
 			}
-			minWidth += (this.foregrounds.length-1) * (minHeight/2 + this.spacing);
+			var spacing = this.getStyleProperty('spacing');
+			minWidth += (this.foregrounds.length-1) * (spacing + 1);
 			return { width: minWidth+2, height: minHeight+2 };
 		}
 	},
@@ -193,22 +206,42 @@ Ui.Container.extend('Ui.Locator',
 			this.border.arrange(0, 0, width, height);
 			return;
 		}
-
-		x = 0;
+		var spacing = this.getStyleProperty('spacing');
+		x = 1;
 		for(var i = 0; i < this.foregrounds.length; i++) {
 			var bg = this.backgrounds[i];
 			var fg = this.foregrounds[i];
 			var fgWidth = fg.getMeasureWidth();
 			fg.arrange(x+1, 0+1, fgWidth, height-2);
 			if(i == 0)
-				bg.arrange(x+1, 0+1, fgWidth + (height-2)/2, height-2);
+				bg.arrange(x, 0+1, fgWidth + spacing, height-2);
 			else if(i == this.foregrounds.length -1)
-				bg.arrange(x - height/2+1, 0+1, fgWidth + (height-2)/2, height-2);
+				bg.arrange(x - spacing, 0+1, fgWidth + spacing, height-2);
 			else
-				bg.arrange(x - height/2+1, 0+1, fgWidth + (height-2), height-2);
-			x += fgWidth + (height-2)/2 + this.spacing;
+				bg.arrange(x - spacing, 0+1, fgWidth + spacing*2, height-2);
+			x += fgWidth + spacing + 1;
 		}
 		this.border.arrange(0, 0, width, height);
+	},
+	
+	onStyleChange: function() {
+		var spacing = this.getStyleProperty('spacing');
+		var radius = this.getStyleProperty('radius');
+		for(var i = 0; i < this.backgrounds.length; i++) {
+			var bg = this.backgrounds[i];
+			if('setArrowLength' in bg)
+				bg.setArrowLength(spacing);
+			bg.setRadius(radius-1);
+		}
+		this.border.setRadius(radius);
+		this.updateColors();
+	}
+}, {
+	style: {
+		color: new Ui.Color({ r: 0.31, g: 0.66, b: 1 }),
+		focusColor: '#f6caa2',
+		radius: 3,
+		spacing: 20
 	}
 });
 
@@ -216,7 +249,8 @@ Ui.Shape.extend('Ui.LocatorRightArrow',
 /**@lends Ui.LocatorRightArrow#*/
 {
 	radius: 8,
-
+	length: 10,
+	
 	/**
 	 * @constructs
 	 * @class
@@ -227,13 +261,20 @@ Ui.Shape.extend('Ui.LocatorRightArrow',
 
 	setRadius: function(radius) {
 		this.radius = radius;
+		this.invalidateArrange();
+	},
+	
+	setArrowLength: function(length) {
+		this.length = length;
+		this.invalidateArrange();
 	}
 }, 
 /**@lends Ui.LocatorRightArrow#*/
 {
 	arrangeCore: function(width, height) {
+		console.log(this+'.arrangeCore length: '+this.length);
 		Ui.LocatorRightArrow.base.arrangeCore.call(this, width, height);
-		var v1 = width - height/2;
+		var v1 = width - this.length;
 		var v2 = height/2;
 		var v3 = height-this.radius;
 		this.setPath('M'+this.radius+',0 L'+v1+',0 L'+width+','+v2+' L'+v1+','+height+' L'+this.radius+','+height+' Q0,'+height+' 0,'+v3+' L0,'+this.radius+' Q0,0 '+this.radius+',0 z');
@@ -244,6 +285,7 @@ Ui.Shape.extend('Ui.LocatorLeftArrow',
 /**@lends Ui.LocatorLeftArrow#*/
 {
 	radius: 8,
+	length: 10,
 
 	/**
 	 * @constructs
@@ -255,6 +297,11 @@ Ui.Shape.extend('Ui.LocatorLeftArrow',
 
 	setRadius: function(radius) {
 		this.radius = radius;
+	},
+	
+	setArrowLength: function(length) {
+		this.length = length;
+		this.invalidateDraw();
 	}
 }, 
 /**@lends Ui.LocatorLeftArrow#*/
@@ -264,13 +311,15 @@ Ui.Shape.extend('Ui.LocatorLeftArrow',
 		var v2 = width - this.radius;
 		var v3 = height - this.radius;
 		var v4 = height/2;
-		this.setPath('M0,0 L'+v2+',0 Q'+width+',0 '+width+','+this.radius+' L'+width+','+v3+' Q'+width+','+height+' '+v2+','+height+' L0,'+height+' L'+v4+','+v4+' z');
+		this.setPath('M0,0 L'+v2+',0 Q'+width+',0 '+width+','+this.radius+' L'+width+','+v3+' Q'+width+','+height+' '+v2+','+height+' L0,'+height+' L'+this.length+','+v4+' z');
 	}
 });
 
 Ui.Shape.extend('Ui.LocatorLeftRightArrow', 
 /**@lends Ui.LocatorLeftRightArrow#*/
 {
+	length: 10,
+
 	/**
 	 * @constructs
 	 * @class
@@ -280,15 +329,20 @@ Ui.Shape.extend('Ui.LocatorLeftRightArrow',
 	},
 
 	setRadius: function(radius) {
+	},
+	
+	setArrowLength: function(length) {
+		this.length = length;
+		this.invalidateDraw();
 	}
 }, 
 /**@lends Ui.LocatorLeftRightArrow#*/
 {
 	arrangeCore: function(width, height) {
 		Ui.LocatorLeftRightArrow.base.arrangeCore.call(this, width, height);
-		var v1 = width - height/2;
+		var v1 = width - this.length;
 		var v2 = height/2;
-		this.setPath('M0,0 L'+v1+',0 L'+width+','+v2+' L'+v1+','+height+' L0,'+height+' L'+v2+','+v2+' z');
+		this.setPath('M0,0 L'+v1+',0 L'+width+','+v2+' L'+v1+','+height+' L0,'+height+' L'+this.length+','+v2+' z');
 	}
 });
 
