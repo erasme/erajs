dd1cf02d1dd021bf1d166b01f452dd050925a507
Daniel Lacroix <dlacroix@erasme.org>
2013-09-16 17:45:42 +0200

    change default style

--- era/ui/styles.js
+++ era/ui/styles.js
@@ -11,10 +11,25 @@ Core.Object.extend('Ui.Styles', {}, {}, {
 			color: '#e4e4e4',
 			focusColor: '#f6caa2'
 		},
+		"Ui.ToggleButton": {
+			spacing: 8,
+			color: '#e4e4e4',
+			focusColor: '#f6caa2',
+			toggleColor: '#dc6c36'
+		},
+		"Ui.SegmentBar": {
+			spacing: 8
+		},
+		"Ui.Locator": {
+			spacing: 10,
+			color: '#e4e4e4',
+			focusColor: '#f6caa2'
+		},
 		"Ui.ScrollingArea": {
-			color: '#9e9e9e',
-			showScrollbar: true,
-			overScroll: true
+			color: '#999999',
+			showScrollbar: false,
+			overScroll: true,
+			radius: 0
 		},
 		"Ui.LinkButton": {
 			spacing: 8,
@@ -34,53 +49,8 @@ Core.Object.extend('Ui.Styles', {}, {}, {
 		"Ui.Separator": {
 			color: '#000000'
 		},
-		"Ui.ToolBar": {
-			"Ui.Label": {
-				fontSize: 16,
-				color: '#ffffff'
-			},
-			"Ui.Button": {
-				color: '#f4f4f4'
-			},
-			"Ui.UploadButton": {
-				color: '#f4f4f4'
-			},
-			"Ui.ToggleButton": {
-				color: '#f4f4f4'
-			},
-			"Ui.TextButtonField": {
-				color: '#f4f4f4'
-			},
-			"Ui.ScrollingArea": {
-				color: '#f4f4f4'
-			}
-		},
-		"Ui.Popup": {
-			color: new Ui.Color({ r: 0.1, g: 0.15, b: 0.2 }),
-			shadowColor: new Ui.Color({ r: 0, g: 0, b: 0, a: 0.5 }),
-
-			"Ui.Label": {
-				color: '#ffffff'
-			},
-			"Ui.Text": {
-				color: '#ffffff'
-			},
-			"Ui.Separator": {
-				color: '#ffffff'
-			},
-			"Ui.MonthCalendar": {
-				color: '#ffffff',
-				dayColor: new Ui.Color({ r: 0.31, g: 0.66, b: 1, a: 0.3 }),
-				currentDayColor: new Ui.Color({ r: 1, g: 0.31, b: 0.66, a: 0.5 })
-			}
-		},
-		"Ui.MenuDialog": {
-			"Ui.Separator": {
-				color: '#666666'
-			}
-		},
 		"Ui.VirtualKeyboardKey": {
-			radius: 8,
+			radius: 3,
 			color: new Ui.Color({ r: 0.9, g: 0.9, b: 0.9 }),
 			focusColor: new Ui.Color({ r: 0.9, g: 0.9, b: 0.9 })
 		}
