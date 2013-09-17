70a7f365db1cc43121d5f4c13ba598dfa76e33fa
Daniel Lacroix <dlacroix@erasme.org>
2013-09-17 12:29:12 +0200

    flatten the design

--- era/ui/menu.js
+++ era/ui/menu.js
@@ -17,8 +17,13 @@ Ui.Pressable.extend('Ui.Menu',
 	constructor: function(config) {
 		this.addEvents('item');
 
-		this.headerLabel = new Ui.Label({ margin: 5, horizontalAlign: 'left' });
-		this.append(this.headerLabel);
+		var vbox = new Ui.VBox();
+		this.append(vbox);
+		
+		this.headerLabel = new Ui.Label({ margin: 5, marginBottom: 0, horizontalAlign: 'left' });
+		vbox.append(this.headerLabel);
+		
+		vbox.append(new Ui.Rectangle({ height: 1, width: 1, fill: '#aaaaaa', marginLeft: 5, marginRight: 5 }));
 
 		this.dialog = new Ui.MenuDialog({ element: this.headerLabel });
 		this.connect(this.dialog, 'item', function(dialog, item) {
@@ -31,7 +36,6 @@ Ui.Pressable.extend('Ui.Menu',
 		if(this.header != header) {
 			this.header = header;
 			this.headerLabel.setText(header);
-			this.dialog.setHeader(header);
 		}
 	},
 
@@ -85,20 +89,17 @@ Ui.Pressable.extend('Ui.Menu',
 	}
 });
 
-Ui.Container.extend('Ui.MenuDialog', 
+Ui.Popup.extend('Ui.MenuDialog', 
 /**@lends Ui.MenuDialog#*/
 {
 	element: undefined,
-	background: undefined,
-	header: undefined,
 	scroll: undefined,
 	content: undefined,
-	shadow: undefined,
 
 	/**
 	 * @constructs
 	 * @class
-	 * @extends Ui.Container
+	 * @extends Ui.Popup
 	 */
 	constructor: function(config) {
 		this.addEvents('item');
@@ -110,31 +111,16 @@ Ui.Container.extend('Ui.MenuDialog',
 		else
 			throw('element property is NEEDED for Ui.MenuDialog');
 
-		this.shadow = new Ui.Rectangle({ opacity: 0, fill: new Ui.Color({ a: 0 }) });
-		this.appendChild(this.shadow);
+		this.setExpandable(true);
+		this.setAutoHide(true);
 
-		this.background = new Ui.MenuBackground({ fill: new Ui.Color({ r: 0.96, g: 0.96, b: 0.96 }), radius: 4 });
-		this.appendChild(this.background);
-
-		this.header = new Ui.Label({ margin: 5, horizontalAlign: 'left' });
-		this.appendChild(this.header);
-
-		this.scroll = new Ui.ScrollingArea({ margin: 5, scrollHorizontal: true });
+		this.scroll = new Ui.ScrollingArea({ scrollHorizontal: true });
 		this.content = new Ui.VBox();
 		this.scroll.setContent(this.content);
-		this.appendChild(this.scroll);
-
-		this.connect(this.getDrawing(), 'mousedown', this.onMouseDown);
-		this.connect(this.scroll.getDrawing(), 'mousedown', function(event) { event.stopPropagation(); });
-		this.connect(this.getDrawing(), 'fingerdown', this.onFingerDown);
-		this.connect(this.scroll.getDrawing(), 'fingerdown', function(event) { event.stopPropagation(); });
+		this.setContent(this.scroll);
 
 		// handle keyboard
 		this.connect(this.getDrawing(), 'keydown', this.onKeyDown);
-	},
-
-	setHeader: function(header) {
-		this.header.setText(header);
 	},
 
 	clearItems: function() {
@@ -159,18 +145,6 @@ Ui.Container.extend('Ui.MenuDialog',
 		var item = menuitem.getContent();
 		this.fireEvent('item', this, item);
 		this.hide();
-	},
-
-	onMouseDown: function(event) {
-		this.hide();
-		event.preventDefault();
-		event.stopPropagation();
-	},
-
-	onFingerDown: function(event) {
-		this.hide();
-		event.preventDefault();
-		event.stopPropagation();
 	},
 
 	onKeyDown: function(event) {
@@ -219,157 +193,9 @@ Ui.Container.extend('Ui.MenuDialog',
 }, 
 /**@lends Ui.MenuDialog#*/
 {
-	visible: false,
-
 	show: function() {
 		var oldVisible = this.getIsVisible();
-		Ui.MenuDialog.base.show.call(this);
-
-		if(!oldVisible) {
-			this.header.setFontWeight(this.element.getFontWeight());
-			this.header.setFontSize(this.element.getFontSize());
-			Ui.App.current.appendDialog(this);
-		}
-	},
-
-	onHidden: function() {
-		Ui.App.current.removeDialog(this);
-	},
-
-	measureCore: function(width, height) {
-		this.shadow.measure(width, height);
-
-		var point1 = this.element.pointToWindow({ x: 0, y: 0 });
-		var point2 = this.element.pointToWindow({ x: this.element.getLayoutWidth(), y: this.element.getLayoutHeight() });
-		
-		var headerSize = this.header.measure(point2.x, point2.y);
-		var scrollSize = this.scroll.measure(width, height - point2.y);
-
-		return { width: Math.max(headerSize.width, scrollSize.width), height: (headerSize.height + scrollSize.height) };
-	},
-
-	arrangeCore: function(width, height) {
-		this.shadow.arrange(0, 0, width, height);
-
-		var point1 = this.element.pointToWindow({ x: 0, y: 0 });
-		var point2 = this.element.pointToWindow({ x: this.element.getLayoutWidth(), y: this.element.getLayoutHeight() });
-
-		this.header.arrange(point1.x - 5, point1.y - 5, this.header.getMeasureWidth(), this.header.getMeasureHeight());
-
-		var offset = 0;
-		if(this.scroll.getMeasureWidth() > width - point1.x - 5)
-			offset = point1.x - (width - this.scroll.getMeasureWidth()) - 5;
-		this.scroll.arrange(point1.x - offset - 5, point2.y, Math.max(this.header.getMeasureWidth(), this.scroll.getMeasureWidth()), this.scroll.getMeasureHeight());
-
-		this.background.setTab(offset, this.header.getMeasureWidth(), this.header.getMeasureHeight() - 5);
-
-		this.background.arrange(point1.x - offset - 5, point1.y - 5, Math.max(this.header.getMeasureWidth(), this.scroll.getMeasureWidth()), this.header.getMeasureHeight() + this.scroll.getMeasureHeight() - 5);
-	}
-});
-
-Ui.Container.extend('Ui.MenuBackground', 
-/**@lends Ui.MenuBackground#*/
-{
-	darkShadow: undefined,
-	lightShadow: undefined,
-	background: undefined,
-
-	radius: 8,
-	fill: 'black',
-	tabOffset: 30,
-	tabWidth: 10,
-	tabHeight: 10,
-
-	/**
-	 * @constructs
-	 * @class
-	 * @extends Ui.Container
-	 */
-	constructor: function(config) {
-
-		this.darkShadow = new Ui.Shape({ fill: '#010002' });
-		this.appendChild(this.darkShadow);
-
-		this.lightShadow = new Ui.Shape({ fill: '#5f625b' });
-		this.appendChild(this.lightShadow);
-
-		var yuv = (new Ui.Color({ r: 0.50, g: 0.50, b: 0.50 })).getYuv();
-		var gradient = new Ui.LinearGradient({ stops: [
-			{ offset: 0, color: new Ui.Color({ y: 0.25, u: yuv.u, v: yuv.v }) },
-			{ offset: 1, color: new Ui.Color({ y: 0.16, u: yuv.u, v: yuv.v }) }
-		] });
-
-		this.background = new Ui.Shape({ fill: gradient });
-		this.appendChild(this.background);
-	},
-
-	setTab: function(offset, width, height) {
-		this.tabOffset = offset;
-		this.tabWidth = width;
-		this.tabHeight = height;
-		this.invalidateArrange();
-	},
-
-	setRadius: function(radius) {
-		if(this.radius != radius) {
-			this.radius = radius;
-			this.invalidateArrange();
-		}
-	},
-
-	setFill: function(fill) {
-		if(this.fill != fill) {
-			this.fill = Ui.Color.create(fill);
-			var yuv = this.fill.getYuva();
-			if(this.getLayoutHeight() > 0) {
-				var gradient = new Ui.LinearGradient({ stops: [
-					{ offset: 0, color: new Ui.Color({ y: yuv.y - 0.1, u: yuv.u, v: yuv.v }) },
-					{ offset: this.tabHeight/(this.getLayoutHeight()*2), color: new Ui.Color({ y: yuv.y, u: yuv.u, v: yuv.v }) },
-					{ offset: 1, color: new Ui.Color({ y: yuv.y, u: yuv.u, v: yuv.v }) }
-				] });
-				this.background.setFill(gradient);
-			}
-			this.darkShadow.setFill(new Ui.Color({ y: yuv.y - 0.4, u: yuv.u, v: yuv.v }));
-			this.lightShadow.setFill(new Ui.Color({ y: yuv.y + 0.1, u: yuv.u, v: yuv.v }));
-		}
-	},
-
-	/**#@+
-	* @private
-	*/
-
-	genPath: function(width, height, radius, tabOffset, tabWidth, tabHeight) {
-		if(tabWidth == width)
-			return 'M'+radius+',0 L'+(width-radius)+',0 Q'+width+',0 '+width+','+radius+' L'+width+','+(height-radius)+' Q'+width+','+height+' '+(width-radius)+','+height+' L'+radius+','+height+' Q'+0+','+height+' 0,'+(height-radius)+' L0,'+radius+' Q0,0 '+radius+',0 z';
-
-		var str = 'M'+tabOffset+','+tabHeight+' L'+tabOffset+','+radius+' Q'+tabOffset+',0 '+(tabOffset+radius)+',0 L'+(tabOffset + tabWidth - radius)+',0 Q'+(tabOffset + tabWidth)+',0 '+(tabOffset + tabWidth)+','+radius+' L'+(tabOffset + tabWidth)+','+tabHeight+' L'+(width - radius)+','+tabHeight+' Q'+width+','+tabHeight+' '+width+','+(tabHeight+radius)+' L'+width+','+(height-radius)+' Q'+width+','+height+' '+(width-radius)+','+height+' L'+radius+','+height+' Q0,'+height+' 0,'+(height-radius);
-		if(tabOffset == 0)
-			str += ' z';
-		else
-			str += ' L0,'+(tabHeight+radius)+' Q0,'+tabHeight+' '+radius+','+tabHeight+' z';
-		return str;
-	}
-	/**#@-*/
-}, 
-/**@lends Ui.MenuBackground#*/
-{
-	arrangeCore: function(width, height) {
-		this.darkShadow.setPath(this.genPath(width, height, this.radius, this.tabOffset, this.tabWidth, this.tabHeight));
-		this.darkShadow.arrange(0, 0, width, height);
-
-		this.lightShadow.setPath(this.genPath(width-2, height-2, this.radius-1, this.tabOffset, this.tabWidth-2, this.tabHeight));
-		this.lightShadow.arrange(1, 1, width - 2, height - 2);	
-		
-		this.background.setPath(this.genPath(width-4, height-4, this.radius - 1.4, this.tabOffset, this.tabWidth-4, this.tabHeight));
-		this.background.arrange(2, 2, width - 4, height - 4);
-
-		var yuv = this.fill.getYuva();
-		var gradient = new Ui.LinearGradient({ stops: [
-			{ offset: 0, color: new Ui.Color({ y: yuv.y - 0.1, u: yuv.u, v: yuv.v }) },
-			{ offset: this.tabHeight/(height*2), color: new Ui.Color({ y: yuv.y, u: yuv.u, v: yuv.v }) },
-			{ offset: 1, color: new Ui.Color({ y: yuv.y, u: yuv.u, v: yuv.v }) }
-		] });
-		this.background.setFill(gradient);
+		Ui.MenuDialog.base.show.call(this, this.element, 'bottom');
 	}
 });
 
@@ -387,10 +213,10 @@ Ui.MouseOverable.extend('Ui.MenuItem',
 	constructor: function() {
 		this.addEvents('press');
 
-		this.background = new Ui.Rectangle({ fill: '#1c8ef2', opacity: 0, radius: 4 });
+		this.background = new Ui.Rectangle({ fill: '#cecece', opacity: 0 });
 		this.append(this.background);
 
-		this.pressable = new Ui.Pressable({ padding: 5, paddingLeft: 10, paddingRight: 10 });
+		this.pressable = new Ui.Pressable({ padding: 10 });
 		this.append(this.pressable);
 		this.connect(this.pressable, 'press', function() {
 			this.fireEvent('press', this);
