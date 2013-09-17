24e59b9d90803191ae684ab26b96435a058603a9
Daniel Lacroix <dlacroix@erasme.org>
2013-09-17 12:30:18 +0200

    use Ui.ContextBar

--- era/ui/dialog.js
+++ era/ui/dialog.js
@@ -13,100 +13,6 @@ Ui.CanvasElement.extend('Ui.DialogGraphic', {}, {
 	}
 });
 
-Ui.HBox.extend('Ui.DialogContextBar', {
-	selection: undefined,
-	counter: undefined,
-	actionsBox: undefined,
-	
-	constructor: function(config) {
-		this.selection = config.selection;
-		delete(config.selection);
-		
-		this.setSpacing(5);
-		
-		var closeButton = new Ui.Pressable({ margin: 10 });
-		closeButton.setContent(
-			new Ui.Icon({ icon: 'close', width: 30, height: 30, verticalAlign: 'center', fill: '#444444' })
-		);
-		this.append(closeButton);
-		this.connect(closeButton, 'press', this.onClosePress);
-		
-		var lbox = new Ui.LBox({ width: 30 });
-		this.append(lbox);
-		this.counter = new Ui.Label({
-			horizontalAlign: 'center', verticalAlign: 'center', fontSize: 24, color: '#444444'
-		});
-		lbox.setContent(this.counter);
-		
-		var sep = new Ui.Separator({ width: 1 });
-		this.append(sep);
-		
-		this.actionsBox = new Ui.HBox({ uniform: true, spacing: 5, horizontalAlign: 'right' });
-		this.append(this.actionsBox, true);
-		
-		this.connect(this.selection, 'change', this.onSelectionChange);
-	},
-	
-	onClosePress: function() {
-		//console.log(this+'.onClosePress '+this.selection);
-		this.selection.clear();
-	},
-	
-	onSelectionChange: function() {
-		this.counter.setText(this.selection.getElements().length);
-		
-		var actions = this.selection.getActions();
-		
-		this.actionsBox.setContent(undefined);
-		var first = true;
-		for(var actionName in actions) {
-			var action = actions[actionName];
-			if(action.hidden === true)
-				continue;
-			var dropbox = new Ui.DropBox();
-			dropbox.dialogContextBarAction = action;
-			dropbox.addMimetype(this.selection.getElements()[0].classType);
-			//console.log('dropbox allow: '+this.selection.getElements()[0].classType);
-			this.connect(dropbox, 'drop', this.onDrop);
-			
-			var pressable = new Ui.Pressable({ margin: 10 });
-			dropbox.setContent(pressable);
-			pressable.dialogContextBarAction = action;
-			this.connect(pressable, 'press', this.onActionPress);
-			var hbox = new Ui.HBox({ spacing: 5 });
-			pressable.setContent(hbox);
-			if(first)
-				first = false;
-			else
-				hbox.append(new Ui.Separator({ width: 1 }));
-			var color;
-			if('color' in action)
-				color = Ui.Color.create(action.color);
-			else
-			 	color = Ui.Color.create('#444444');
-			hbox.append(new Ui.Icon({ icon: action.icon, width: 30, height: 30, verticalAlign: 'center', fill: color }));
-			hbox.append(new Ui.Label({ text: action.text, horizontalAlign: 'center', verticalAlign: 'center', fontWeight: 'bold', color: color }));
-			this.actionsBox.append(dropbox);
-		}
-	},
-	
-	onActionPress: function(pressable) {
-		var action = pressable.dialogContextBarAction;
-		var scope = this;
-		if('scope' in action)
-			scope = action.scope;
-		action.callback.call(scope, this.selection);
-	},
-
-	onDrop: function(dropbox) {	
-		var action = dropbox.dialogContextBarAction;
-		var scope = this;
-		if('scope' in action)
-			scope = action.scope;
-		action.callback.call(scope, this.selection);
-	}
-});
-
 Ui.Container.extend('Ui.Dialog', {
 	dialogSelection: undefined,
 	bg: undefined,
@@ -161,7 +67,7 @@ Ui.Container.extend('Ui.Dialog', {
 		this.actionBg = new Ui.Rectangle({ fill: '#e8e8e8' });
 		lbox.append(this.actionBg);
 
-		this.contextBox = new Ui.DialogContextBar({ selection: this.dialogSelection });
+		this.contextBox = new Ui.ContextBar({ selection: this.dialogSelection });
 		this.contextBox.hide();
 		lbox.append(this.contextBox);
 		
@@ -172,7 +78,7 @@ Ui.Container.extend('Ui.Dialog', {
 		this.cancelBox = new Ui.LBox();
 		this.actionBox.append(this.cancelBox);
 
-		this.actionButtonsBox = new Ui.HBox({ uniform: true, horizontalAlign: 'right' });
+		this.actionButtonsBox = new Ui.HBox({ spacing: 5, uniform: true, horizontalAlign: 'right' });
 		this.actionBox.append(this.actionButtonsBox, true);
 		
 		this.connect(this.dialogSelection, 'change', this.onDialogSelectionChange);		
