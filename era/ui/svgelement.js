f2a5529e0ca5060a61ea50c415d98b78754cdcdc
Daniel Lacroix <dlacroix@erasme.org>
2014-06-18 14:36:06 +0200

    provide renderSVG for SVGElement

--- era/ui/svgelement.js
+++ era/ui/svgelement.js
@@ -1,13 +1,13 @@
-Ui.Element.extend('Ui.SVGElement', {}, 
-/**@name Ui.SVGElement
- * @class
- * @extends Ui.Element
- */
-/**@lends Ui.SVGElement*/
-{
+Ui.Element.extend('Ui.SVGElement', {
+	renderSVG: function(svg) {
+	}
+}, {
 	renderDrawing: function() {
 		var svg = document.createElementNS(svgNS, 'svg');
 		svg.setAttribute('focusable', false);
+		var content = this.renderSVG(svg);
+		if(content !== undefined)
+			svg.appendChild(content);
 		return svg;
 	}
 });
