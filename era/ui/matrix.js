631ee89401f9bcc2ce98e7bbf3132c580237236e
Daniel Lacroix <dlacroix@erasme.org>
2014-08-19 17:17:25 +0200

    round the translate part of matrix to improve image sharpness

--- era/ui/matrix.js
+++ era/ui/matrix.js
@@ -115,7 +115,7 @@ Core.Object.extend('Ui.Matrix',
 /**@lends Ui.Matrix#*/
 {
 	toString: function() {
-		return 'matrix('+this.a.toFixed(4)+','+this.b.toFixed(4)+','+this.c.toFixed(4)+','+this.d.toFixed(4)+','+this.e.toFixed(4)+','+this.f.toFixed(4)+')';
+		return 'matrix('+this.a.toFixed(4)+','+this.b.toFixed(4)+','+this.c.toFixed(4)+','+this.d.toFixed(4)+','+this.e.toFixed(0)+','+this.f.toFixed(0)+')';
 	}
 }, 
 /**@lends Ui.Matrix*/
