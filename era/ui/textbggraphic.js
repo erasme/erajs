28d7fab6b0797d7cc8d274713e292669dadb1a4d
Daniel Lacroix <dlacroix@erasme.org>
2015-02-24 17:42:52 +0100

    change the look the text fields for a more web classic one

--- era/ui/textbggraphic.js
+++ era/ui/textbggraphic.js
@@ -23,6 +23,8 @@ Ui.CanvasElement.extend('Ui.TextBgGraphic', {
 		var w = this.getLayoutWidth();
 		var h = this.getLayoutHeight();
 
+		var radius = this.getStyleProperty('radius');
+		radius = Math.max(0, Math.min(radius, Math.min(w/2, h/2)));
 		var borderWidth = this.getStyleProperty('borderWidth');
 		
 		var lh = Math.max(8, h-4-16);
@@ -33,16 +35,24 @@ Ui.CanvasElement.extend('Ui.TextBgGraphic', {
 		
 		ctx.fillStyle = this.getBackground().getCssRgba();
 		ctx.beginPath();
-		ctx.moveTo(0, h-lh-4);
+
+		ctx.roundRect(0, 0, w, h, radius, radius, radius, radius);
+
+		/*ctx.moveTo(0, h-lh-4);
 		ctx.lineTo(0, h-4);
 		ctx.lineTo(w, h-4);
 		ctx.lineTo(w, h-8-4+borderWidth);
 		ctx.lineTo(w-borderWidth, h-8-4+borderWidth);
 		ctx.lineTo(w-borderWidth, h-4-borderWidth);
 		ctx.lineTo(borderWidth, h-4-borderWidth);
-		ctx.lineTo(borderWidth, h-lh-4);
+		ctx.lineTo(borderWidth, h-lh-4);*/
+
+		/*ctx.moveTo(0, h-4);
+		ctx.lineTo(w, h-4);
+		ctx.lineTo(w, h-4-borderWidth);
+		ctx.lineTo(0, h-4-borderWidth);*/
 		ctx.closePath();
-		ctx.fill();	
+		ctx.fill();
 	},
 	
 	onDisable: function() {
@@ -63,6 +73,7 @@ Ui.CanvasElement.extend('Ui.TextBgGraphic', {
 	}
 }, {
 	style: {
+		radius: 3,
 		borderWidth: 2,
 		background: '#444444',
 		focusBackground: '#21d3ff'
