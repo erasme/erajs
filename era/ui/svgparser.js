8e56081a524f5da22ba2c5e17a5deaa3231419ea
Daniel Lacroix <dlacroix@erasme.org>
2015-10-19 18:02:45 +0200

    improve SVG Path parser for compact notation

--- era/ui/svgparser.js
+++ era/ui/svgparser.js
@@ -24,25 +24,35 @@ Core.Object.extend('Ui.SvgParser', {
 		if(!this.end) {
 			while((this.pos < this.path.length) && ((this.path[this.pos] == ' ') || (this.path[this.pos] == ',') || (this.path[this.pos] == ';')))
 				this.pos++;
+			var dotseen = false;
+			var eseen = false;
 			this.current = '';
-			var lastIsText;
-			while((this.pos < this.path.length) && (this.path[this.pos] != ' ') && (this.path[this.pos] != ',') && (this.path[this.pos] != ';')) {
-				var c = this.path[this.pos];
-				var isText = (c != 'e') && ((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z'));
-				if((lastIsText === undefined) || (lastIsText == isText)) {
-					lastIsText = isText;
-					this.current += c;
-					this.pos++;
-				}
-				else
-					break;
-			}
-			var value = parseFloat(this.current);
-			this.value = !isNaN(value);
-			if(this.value)
-				this.current = value;
-			else
+			var c = this.path[this.pos];
+
+			var isCmd = (c != 'e') && ((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z'));
+			if(isCmd) {
+				this.current = this.path[this.pos++];
 				this.cmd = this.current;
+				this.value = false;
+			}
+			else {
+				while((this.pos < this.path.length) && (((this.path[this.pos] >= '0') && (this.path[this.pos] <= '9')) ||
+				       ((this.path[this.pos] == '-') && (this.current.length == 0)) || (!eseen && (this.path[this.pos] == 'e')) || (!dotseen && (this.path[this.pos] == '.')))) {
+					if(this.path[this.pos] == '.')
+						dotseen = true;
+					if(this.path[this.pos] == 'e')
+						eseen = true;
+					this.current += this.path[this.pos++];
+				}
+				this.value = true;
+				if(this.current[0] == '.')
+					this.current = '0'+this.current;
+				if((this.current[0] == '-') && (this.current[1] == '.'))
+					this.current = '-0'+this.current.substring(1);
+				this.current = parseFloat(this.current);
+				if(isNaN(this.current))
+					throw('bad number');
+			}
 		}
 	},
 
