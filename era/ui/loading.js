a7a206be23b6f55f76130969051db8af7238497e
Daniel Lacroix <dlacroix@erasme.org>
2015-10-19 18:08:37 +0200

    change loading animation

--- era/ui/loading.js
+++ era/ui/loading.js
@@ -1,29 +1,30 @@
-Ui.LBox.extend('Ui.Loading', 
-/**@lends Ui.Loading#*/
-{
-	icon: undefined,
+Ui.CanvasElement.extend('Ui.Loading', {
+	fill: 'black',
 	clock: undefined,
+	ease: undefined,
 
-    /**
-     * @constructs
-	 * @class   
-     * @extends Ui.LBox
-     */ 
 	constructor: function(config) {
-		this.icon = new Ui.Icon({ icon: 'loading' });
-		this.icon.setTransformOrigin(0.5, 0.5);
-		this.append(this.icon);
+		if('fill' in config) {
+			this.setFill(config.fill);
+			delete(config.fill);
+		}
+		else
+			this.setFill(this.fill);
+
+		this.ease = new Anim.PowerEase({ mode: 'inout' });
 		this.clock = new Anim.Clock({ repeat: 'forever', duration: 2 });
 		this.connect(this.clock, 'timeupdate', this.onTick);
 	},
 
-	/**@private*/
+	setFill: function(fill) {
+		this.fill = Ui.Color.create(fill);
+		this.invalidateDraw();
+	},
+
 	onTick: function(clock, progress) {
-		this.icon.setTransform(Ui.Matrix.createRotate(progress * 360));
+		this.invalidateDraw();
 	}
-}, 
-/**@lends Ui.Loading#*/
-{
+}, {
 	onVisible: function() {
 		Ui.Loading.base.onVisible.apply(this, arguments);
 		this.clock.begin();
@@ -34,16 +35,34 @@ Ui.LBox.extend('Ui.Loading',
 		this.clock.stop();
 	},
 
-	onStyleChange: function() {
-		this.icon.setFill(this.getStyleProperty('color'));
+	updateCanvas: function(ctx) {
+		var p = this.clock.getProgress();
+		var p2 = (p > 0.8) ? (1 - ((p - 0.8)*5)) : (p / 0.8);
+
+		var w = this.getLayoutWidth();
+		var h = this.getLayoutHeight();
+		var x = w / 2;
+		var y = h / 2;
+		var lineWidth = Math.max(2, Math.min(5, Math.min(w, h) * 5 / 60));
+		var radius = ((Math.min(w, h) - lineWidth) / 2) - 5;
+		var startAngle = Math.PI * 2 * p;
+		if(p > 0.8)
+			startAngle = Math.PI * 2 * p - (Math.PI * 2 * 5 * this.ease.ease(p2) / 6);
+		var endAngle = startAngle + (Math.PI / 4) + (Math.PI * 2 * 5 * this.ease.ease(p2) / 6);
+
+		ctx.strokeStyle = this.getStyleProperty('color').getCssRgba();
+		ctx.beginPath();
+		ctx.arc(x, y, radius, startAngle, endAngle, false);
+		ctx.lineWidth = lineWidth;
+		ctx.stroke();
+	},
+
+	measureCore: function(width, height) {
+		return { width: 30, height: 30 };
 	}
-}, 
-/**@lends Ui.Loading*/
-{
+}, {
 	style: {
-		color: new Ui.Color({ r: 0.39, g: 0.39, b: 0.39 })
+		color: new Ui.Color({ r: 0.27, g: 0.52, b: 0.9 })
 	}
 });
-
-
 
