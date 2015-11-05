b02cc308b6ab76bd8ac7b8a3e29cca34cf65f037
Daniel LACROIX <dlacroix@erasme.org>
2015-11-05 17:55:04 +0100

    allow textTransform on Ui.Text

--- era/ui/text.js
+++ era/ui/text.js
@@ -3,5 +3,10 @@ Ui.Html.extend('Ui.Text', {
 	constructor: function(config) {
 		this.getDrawing().style.whiteSpace = 'pre-wrap';
 		this.setSelectable(false);
+	},
+
+	setTextTransform: function(textTransform) {
+		this.getDrawing().style.textTransform = textTransform;
+		this.invalidateMeasure();
 	}
 });
